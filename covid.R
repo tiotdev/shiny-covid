1af0e59f1835030e534a8c6055ffe8eb60ba37bd
tiotdev <tiotdev@tiot.de>
2021-01-20 14:19:42 +0100

    Relative daten

--- covid.R
+++ covid.R
@@ -1,7 +1,11 @@
 library(shiny)
 
+# Datenquelle: https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0/data?orderBy=Meldedatum
 sourcedata <- read.csv("./data/RKI_COVID19-20210120.csv", stringsAsFactors = FALSE)
-# https://stackoverflow.com/questions/51867390/r-programming-filtering-data-frame-with-date-column
+# stringsAsFactors zum Filtern nach Meldedatum https://stackoverflow.com/questions/51867390/r-programming-filtering-data-frame-with-date-column
+
+einwohnerzahlen <- read.csv("./data/Einwohnerzahlen.csv")
+# Quellehttps://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Bevoelkerungsstand/Tabellen/bevoelkerung-nichtdeutsch-laender.html
 
 laenderdaten <- aggregate(sourcedata$AnzahlFall, list(Bundesland = sourcedata$Bundesland), sum)
 
@@ -24,18 +28,34 @@ ui <- fluidPage(
               max = as.Date("2021-01-19","%Y-%m-%d"),
               value=c(as.Date("2020-01-01"),as.Date("2020-12-31")),
               timeFormat="%Y-%m-%d"),
+  # https://shiny.rstudio.com/reference/shiny/latest/radioButtons.html
+  radioButtons("datentyp", "Datentyp:",
+               c("Absolut" = "absolut",
+                 "Relativ" = "relativ")),
   plotlyOutput(outputId = "p")
 )
 
 server <- function(input, output, session, ...) {
   output$p <- renderPlotly({
+    # Quelle: https://plotly-r.com/linking-views-with-shiny.html 17.1.2
     req(input$bl)
     if (identical(input$bl, "")) return(NULL)
     datenrange <- filter(sourcedata, Meldedatum>=as.Date(input$zeitraum[1]) & Meldedatum<=input$zeitraum[2])
     bl <- aggregate(datenrange$AnzahlFall, list(Bundesland = datenrange$Bundesland), sum)
-    gefilterteDaten <- filter(bl, Bundesland %in% input$bl) # Quelle: https://plotly-r.com/linking-views-with-shiny.html 17.1.2
-    layout(plot_ly(gefilterteDaten,x=gefilterteDaten$Bundesland,y=gefilterteDaten$x), title="Covid Fallzahlen nach Bundesland",
-           xaxis=list(title = "Bundesland"),yaxis=list(title = "Gesamtanzahl der Fälle"))
+    gefilterteFallzahlen <- filter(bl, Bundesland %in% input$bl) # Quelle: https://plotly-r.com/linking-views-with-shiny.html 17.1.2
+    gefilterteEinwohner <- filter(einwohnerzahlen, Bundesland %in% input$bl)
+    gefilterteDaten <- data.frame(append(gefilterteFallzahlen, gefilterteEinwohner))
+    print(gefilterteDaten)
+    title <- "Gesamtanzahl der Fälle"
+    fallZahlen <- gefilterteDaten$x
+    if(input$datentyp == "relativ") {
+      title <- "Faelle pro 100.000 Einwohner"
+      print(gefilterteDaten$x)
+      print(gefilterteDaten$Einwohner)
+      fallZahlen <- (gefilterteDaten$x/gefilterteDaten$Einwohner)*100000
+    }
+    layout(plot_ly(gefilterteDaten,x=gefilterteDaten$Bundesland,y=fallZahlen), title="Covid Neuerkrankungen nach Bundesland",
+           xaxis=list(title = "Bundesland"),yaxis=list(title = title))
   })
 }
 
